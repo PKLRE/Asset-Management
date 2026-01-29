66880b587302ed997cf749b05033fa476bde2360
Caxerion <kennethgeveny1@gmail.com>
2026-01-29 13:55:12 +0700

    feat: add database seeders and fixing timestamp conflicts within: -0001_01_01_000000_create_users_table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,7 +12,9 @@ return new class extends Migration {
 
             $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->rememberToken();
 
             // role (flexible, avoid enum lock-in)
             $table->string('role')->default('staff');
